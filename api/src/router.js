f86d79e34b0f24d9879af5e6db9f43671edcdc4c
MohammedYehia <m-ya@windowslive.com>
2021-01-03 18:30:48 +0200

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,10 +1,12 @@
 /* eslint-disable require-jsdoc */
 const {Router} = require('express');
 const multer = require('multer');
+const path = require('path');
 const router = Router();
 const storage = multer.diskStorage({destination: 'api/uploads/', filename});
 const upload = multer({fileFilter: fileFilter, storage});
 
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 function filename(req, file, cb) {
   cb(null, file.originalname);
@@ -26,5 +28,8 @@ router.post('/upload', upload.single('photo'), (req, res)=> {
   res.status(201).json({success: true});
 });
 
+router.get('/photo-viewer', (req, res) => {
+  res.sendFile(photoPath);
+});
 
 module.exports = router;
